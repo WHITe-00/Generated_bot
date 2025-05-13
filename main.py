13382681e798610327c7a2946064a744b6397d5b
Absolut2526 <danyierlonyzh[k2009@gnil.on>
2025-05-14 00:18:32 +0300

    zaporozhets

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+import aiogram
+import asyncio
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+TOKEN = '7545227996:AAGK3EJjl3cri0br_CM-8Cm1ijlIPKfc35o'
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f'Hello')
+
+async def main() -> None:
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
