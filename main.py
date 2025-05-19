33e860756db1b6a42a5ace847c5e05211f6e7cb4
White-00 <ostapvasecko@gmail.com>
2025-05-19 23:49:17 +0300

    commit

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import aiogram
 import asyncio
+import os
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from dotenv import load_dotenv, find_dotenv
 
-TOKEN = '8152070548:AAGYQ3FolODRG-0urMRR0dVtXnYJj5G7Y1I'
+load_dotenv(find_dotenv())
 
-dp = Dispatcher()
+dp = Dispatcher(os.getenv('TOKEN'))
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
     await message.answer(f'Hello, World!')
+
 
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
